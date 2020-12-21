67b18249c9c56d197c879c0e1b0d89170d61b34c
Guilferrari <guilferrari@hotmail.com>
2020-12-21 22:47:17 +0000

    methods

--- src/Ball.java
+++ src/Ball.java
@@ -8,4 +8,21 @@ public class Ball {
     Ball(){
 
     }
+
+    public void setXDirection(int randomXDirection){
+
+    }
+
+    public void setYDirection(int randomYDirection){
+
+    }
+
+    public void move(){
+
+    }
+
+    public void draw(Graphics g){
+
+    }
+
 }
