23270cfb41507a8568928c2ef80903e9f5b5ad9b
Guilferrari <guilferrari@hotmail.com>
2021-01-01 21:46:02 +0000

    keypressed paddle

--- src/Paddle.java
+++ src/Paddle.java
@@ -7,6 +7,7 @@ public class Paddle extends Rectangle {
 
     int id;
     int yVelocity;
+    int speed = 10;
 
     Paddle(int x, int y, int PADDLE_WIDTH, int PADDLE_HEIGHT, int id){
         super(x, y, PADDLE_WIDTH, PADDLE_HEIGHT);
@@ -14,6 +15,27 @@ public class Paddle extends Rectangle {
     }
 
     public void keyPressed(KeyEvent e){
+
+        switch(id){
+            case 1:
+                if(e.getKeyCode()==KeyEvent.VK_W){
+                    setYDirection(-speed);
+                    move();
+                }
+                if(e.getKeyCode()==KeyEvent.VK_S){
+                    setYDirection(speed);
+                    move();
+                }
+            case 2:
+                if(e.getKeyCode()==KeyEvent.VK_UP){
+                    setYDirection(-speed);
+                    move();
+                }
+                if(e.getKeyCode()==KeyEvent.VK_DOWN){
+                    setYDirection(speed);
+                    move();
+                }
+        }
 
     }
 
