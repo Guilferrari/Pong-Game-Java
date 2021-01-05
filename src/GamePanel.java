c9e26b8de7d1862e95299208d651a7bcf756fbe7
Guilferrari <guilferrari@hotmail.com>
2021-01-05 22:38:56 +0000

    move paddle

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -57,7 +57,9 @@ public class GamePanel extends JPanel implements Runnable {
     }
 
     public void move(){
-
+        paddle1.move();
+        paddle2.move();
+        ball.move();
     }
 
     public void checkCollision(){
